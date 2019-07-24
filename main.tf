db99ef7ef8b5bd89377f1c461f4d8b0f1887e7e2
roguetech <patrick.roughan@roguetechnology.org>
2019-07-24 19:26:24 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-state-hfg65"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-west-1"
   }
 }
 
 # Use AWS Terraform provider
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
 # Create EC2 instance
